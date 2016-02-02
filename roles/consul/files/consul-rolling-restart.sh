0b94b6571aacf5c0af8e8dcf4f3083aefd5102b9
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2016-02-03 01:03:29 +1000

    consul: apply rolling restart only to server nodes
    - restart key is registered by leader only when the member is not of
      role node
    - restart key is deleted once member is up for good measure
    - restart is immediate if agent is not configured as a server

--- roles/consul/files/consul-rolling-restart.sh
+++ roles/consul/files/consul-rolling-restart.sh
@@ -33,9 +33,14 @@ fi
 
 me=$(consul-cli agent-self ${ccargs} | jq -r .Member.Addr)
 leader=$(consul-cli status-leader ${ccargs} | cut -f 1 -d ':')
+server=$(consul-cli agent-self ${ccargs}  | jq -r ".Config.Server")
 
 if [ -z "${me}" ]; then
 	echo "Not a consul server."
+	exit 0
+elif [ "${server}" == "false" ]; then
+	# this is a client short-circuit restart
+	systemctl restart consul
 	exit 0
 fi
 
@@ -55,7 +60,7 @@ if [ "${me}" != "$leader" ]; then
 	# Not the leader. Set a watch on /v1/kv/secure/$me/restart. When that
 	# changes, restart consul
 	#
-	consul-cli kv-watch ${ccargs} secure/${me}/restart >/dev/nul
+	consul-cli kv-watch ${ccargs} secure/${me}/restart >/dev/null
 	if [ $? -ne 0 ]; then
 		echo "Error watching restart key"
 		exit 1
@@ -76,15 +81,17 @@ for i in $(consul-cli agent-members ${ccargs} | \
 		sort -r -t ':' -k 3); do
 	node=$(echo ${i} | cut -f 1 -d ':')
 	ip=$(echo ${i} | cut -f 2 -d ':')
+	role=$(echo ${i} | cut -f 3 -d ':')
 
 	if [ -z "${node}" -o -z "${ip}" ]; then
 		echo "Bad return from `consul-cli agent-members`: ${i}"
 		exit 1
 	fi
 
-	if [ "${ip}" != "${me}" ]; then
+	if [ "${role}" != "node" -a "${ip}" != "${me}" ]; then
 		consul-cli kv-write ${ccargs} secure/${ip}/restart restart
 		wait_for_upness ${node}
+		consul-cli kv-delete ${ccargs} secure/${ip}/restart restart
 	fi
 done
 
